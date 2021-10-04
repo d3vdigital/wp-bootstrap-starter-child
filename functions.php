eaf763e5e15e81b537c0f7d55e5b3830a2eb1f7c
Zac Dreyer <zacdreyer@users.noreply.github.com>
2021-10-04 02:13:49 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,19 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+/**
+ * WP Bootstrap Starter  Child functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WP_Bootstrap_Starter_Child
+ */
+
+/**
+ * Queue 'parent-style' stylesheet.
+ */
+add_action('wp_enqueue_scripts', 'enqueue_parent_styles');
 function enqueue_parent_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
+
+
 ?>
