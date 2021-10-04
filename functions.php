0e45285a716f0d8601c4a4f8d58826af8beb317b
Zac Dreyer <zacdreyer@users.noreply.github.com>
2021-10-04 02:14:07 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * WP Bootstrap Starter  Child functions and definitions
+ * WP Bootstrap Starter Child functions and definitions
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
